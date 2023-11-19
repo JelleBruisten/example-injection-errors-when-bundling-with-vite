f51746dd575205b727947baed74552766282aaca
Jelle Bruisten <jelle_bruisten@hotmail.com>
2023-11-19 21:53:22 +0100

    example: injection errors when serving with vite

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AsyncPipe } from '@angular/common';
+import { of } from 'rxjs';
+
+@Injectable({ providedIn: 'root'})
+export class MessageService {
+  message() {
+    return of('hello world');
+  }
+}
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, AsyncPipe],
   selector: 'example-angular-esbuild-secondary-entry-points-build-failure-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'main';
+
+  service = inject(MessageService);
 }
